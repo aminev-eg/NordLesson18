c759274c5b242f41b042805d31c42f3f9a638f49
Eugene Seager <eugene.seager@gmail.com>
2019-11-10 12:43:37 +0300

    Add another comment

--- app/src/main/java/com/reactivelabs/androidapp/FirstFragment.kt
+++ app/src/main/java/com/reactivelabs/androidapp/FirstFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 
 class FirstFragment : Fragment() {
@@ -13,10 +14,13 @@ class FirstFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // sample comment
+        // another comment
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        Toast.makeText(context, "Hello", Toast.LENGTH_SHORT).show()
         super.onViewCreated(view, savedInstanceState)
     }
 }
